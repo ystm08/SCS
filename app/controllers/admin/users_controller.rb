e983e382320111e338c9f6649a887e80c1c9fb8b
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-02-19 07:50:30 +0000

    Add adminレイアウト

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.page(params[:page]).per(2)
+    @users = User.page(params[:page]).per(5)
   end
 
   def show
