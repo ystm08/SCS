9ea930f8fde76ac59567ad6f9fe87bd6938752fd
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-03-17 01:24:00 +0000

    Add通知機能

--- db/migrate/20240316141148_create_notifications.rb
+++ db/migrate/20240316141148_create_notifications.rb
@@ -1,6 +1,7 @@
 class CreateNotifications < ActiveRecord::Migration[6.1]
   def change
     create_table :notifications do |t|
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
