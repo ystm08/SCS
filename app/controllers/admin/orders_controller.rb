bbab5581791890afe7b309af8d7a4131e3fdffc0
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-03-20 14:04:47 +0000

    Add adminフラッシュメッセージ編集

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,17 +5,17 @@ class Admin::OrdersController < ApplicationController
   def show
     @order_details = @order.order_details
   if @order_details.empty?
-    flash[:notice] = "注文履歴がありません"
+    flash[:info] = "注文履歴がありません"
     redirect_to admin_top_path
   end
   end
 
   def update_status
     if @order.update(order_params)
-      flash[:notice] = "注文ステータスを更新しました"
+      flash[:success] = "注文ステータスを更新しました"
       redirect_to request.referer
     else
-      flash[:notice] = "注文ステータスの更新に失敗しました"
+      flash[:success] = "注文ステータスの更新に失敗しました"
       redirect_to request.referer
     end
   end
