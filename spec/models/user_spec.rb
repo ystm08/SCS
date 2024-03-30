261ccf321333251c8b95a59e9751bd350d624e11
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-03-30 06:11:18 +0000

    Add　RSpecテスト項目追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,31 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
     end
+    context '新規登録できない場合' do
+      it '名前が空では登録できない' do
+        @user.name = ''
+        expect(@user).to be_invalid
+        expect(@user.errors.full_messages).to include('名前を入力してください')
+      end
 
+      it 'フリガナが空では登録できない' do
+        @user.name_kana = ''
+        expect(@user).to be_invalid
+        expect(@user.errors.full_messages).to include('フリガナを入力してください')
+      end
+
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        expect(@user).to be_invalid
+        expect(@user.errors.full_messages).to include('Eメールを入力してください')
+      end
+      
+      it 'フリガナが空では登録できない' do
+        @user.name_kana = ''
+        expect(@user).to be_invalid
+        expect(@user.errors.full_messages).to include('フリガナを入力してください')
+      end
+    end
 
   end
 end
