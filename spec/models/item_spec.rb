495b9b1a63d1f37eebf90a1a6440d7c0e3bc4481
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-04-03 14:22:01 +0000

    Add RSpec Itemモデル

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Itemモデルのテスト', type: :model do
+
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
   describe 'バリデーションのテスト' do
     subject { item.valid? }
-    
-    let 
+
+    context '空欄でないこと' do
+      it '商品名が空欄では登録できない' do
+        @item.name = ''
+        expect(@item).to be_invalid
+        expect(@item.errors.full_messages).to include('商品名を入力してください')
+      end
+
+      it '商品説明が空欄では登録できない' do
+        @item.introduction = ''
+        expect(@item).to be_invalid
+        expect(@item.errors.full_messages).to include('商品説明を入力してください')
+      end
+    end
   end
-  
+
 end
