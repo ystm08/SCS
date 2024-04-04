8738a48f8e8199a19cb99f3018fcfe89d7f8b5a0
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-04-04 11:38:29 +0000

    Add ItemモデルRSpec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,6 +10,14 @@ RSpec.describe 'Itemモデルのテスト', type: :model do
     subject { item.valid? }
 
     context '空欄でないこと' do
+      # TODO:item_imageのバリデーションテスト記述変更
+      # it '商品画像が選択されていないと登録できない' do
+      #   @item.item_image.attach(io: File.open('app/images/no_image.jpg'), filename: 'no_image.jpg')
+      #   @item.item_image = ''
+      #   expect(@item).to be_invalid
+      #   expect(@item.errors.full_messages).to include('商品画像を入力してください')
+      # end
+
       it '商品名が空欄では登録できない' do
         @item.name = ''
         expect(@item).to be_invalid
@@ -21,7 +29,17 @@ RSpec.describe 'Itemモデルのテスト', type: :model do
         expect(@item).to be_invalid
         expect(@item.errors.full_messages).to include('商品説明を入力してください')
       end
+
+      it 'カテゴリーが空欄では登録できない' do
+        @item.category_id = ''
+        expect(@item).to be_invalid
+      end
+
+      it '税抜き価格が空欄では登録できない' do
+        @item.price = ''
+        expect(@item).to be_invalid
+        expect(@item.errors.full_messages).to include('税抜価格を入力してください')
+      end
     end
   end
-
 end
